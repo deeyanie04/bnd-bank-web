734130ed59cd8d99192854c41f4f4e7e09e843be
deeyanie04 <dianne2000camu@gmail.com>
2025-12-28 13:28:42 +0800

    Update bank.php

--- bank.php
+++ bank.php
@@ -44,6 +44,231 @@ $stmt->close();
 </head>
 <body>
 
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+        text-decoration: none;
+        font-family: 'Josefin Sans', sans-serif;
+    }
+
+    body {
+        background-color: #f3f5f9;
+    }
+
+    .wrapper {
+        display: flex;
+        position: relative;
+    }
+
+    .wrapper .sidebar {
+        width: 300px;
+        height: 100%;
+        background: #e2688a;
+        padding: 30px 0px;
+        position: fixed;
+    }
+
+    .wrapper .sidebar ul li img {
+        width: 20px;
+        float: left;
+    }
+
+    .wrapper .sidebar h2 {
+        color: #fff;
+        text-transform: uppercase;
+        text-align: center;
+        margin-bottom: 30px;
+    }
+
+    .wrapper .sidebar ul li {
+        padding: 25px;
+        border-bottom: 1px solid #bdb8d7;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.05);
+        border-top: 1px solid rgba(255, 255, 255, 0.05);
+        text-indent: 10px;
+    }
+
+    .wrapper .sidebar ul li a {
+        color: white;
+        display: block;
+    }
+
+    .wrapper .sidebar ul li a .fas {
+        width: 25px;
+    }
+
+    .wrapper .sidebar ul li:hover {
+        background-color: #d85375;
+    }
+
+    .wrapper .sidebar ul li:hover a {
+        color: #fff;
+    }
+
+    .wrapper .sidebar .social_media {
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        display: flex;
+    }
+
+    .wrapper .sidebar .social_media a {
+        display: block;
+        width: 40px;
+        background: #d85375;
+        height: 40px;
+        line-height: 45px;
+        text-align: center;
+        margin: 0 5px;
+        color: #fff;
+        border-top-left-radius: 5px;
+        border-top-right-radius: 5px;
+    }
+
+    .wrapper .main_content {
+        width: 100%;
+        margin-left: 300px;
+    }
+
+    .wrapper .main_content .header {
+        margin: 5%;
+        padding: 30px;
+        color: #717171;
+        border-bottom: 1px solid #e0e4e8;
+    }
+
+
+    .wrapper .main_content .header .name{
+        width: 100%;
+        color:#d85375;
+        justify-content: left;
+         font-size: 25px;
+         font-weight: bold;
+    }
+
+    .wrapper .main_content .info {
+        margin: 20px;
+        color: #717171;
+        line-height: 25px;
+    }
+
+    .wrapper .main_content .info div {
+        margin-bottom: 20px;
+    }
+
+    table {
+        margin: 5%;
+        width: 90%;
+       
+    }
+
+    th, td {
+        border: 1px solid whitesmoke;
+        background-color: #e2688a;
+        margin: 5%;
+        padding: 5%;
+        text-align: left;
+    }
+
+    .wrapper .main_content .info .transaction_box {
+        margin: 5%;
+        margin-top: 0%;
+        padding: 4%;
+        border: 2px solid #e2688a;
+        display: flex;
+        justify-content: space-between; /* Align to the space between elements */
+        align-items: center; /* Align vertically centered */
+    }
+
+    .transaction_box .transaction-text {
+        padding-top: 1%;
+        margin-right: 30%; /* Adjust the margin as needed */
+        font-size: 20px;
+    }
+
+    .transaction_box .submit-button {
+        padding: 10px;
+        font-size: 16px;
+        background-color: #d85375;
+        color: #fff;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    #logo {
+        margin-left: 15%;
+        margin-bottom: 5%;
+        max-width: 200px;
+    }
+
+    .transaction_box #accountNumber {
+        margin-right:5%;
+    }
+
+    #accountNumber {
+        padding-top: 5%;
+        color:#f3f5f9;
+        font-size: 16px;
+        font-weight: bold;
+        margin-bottom: 0%;
+    }
+
+    .number {
+        font-size: 16px;
+        color:#f3f5f9;
+        margin-top: -10%;
+        margin-left: 35%;
+    }
+
+    .balance {
+        color:#f3f5f9;
+        font-size: 16px;
+        font-weight: bold;
+
+    }
+    
+    .hide-transaction-history {
+        color: #d85375;
+        cursor: pointer;
+        float: right;
+        margin-right: 20px;
+        margin-top: 10px;
+    }
+
+    .transaction-history {
+        display: none;
+        margin-top: 20px;
+    }
+
+    .transaction-history table {
+        width: 70%; /* Adjust the width as needed */
+        border-collapse: collapse;
+        margin-top: 10px;
+    }
+
+    .transaction-history th, .transaction-history td {
+        border: 1px solid whitesmoke;
+        background-color: #e2688a;
+        padding: 15px;
+        text-align: left;
+        color: #fff;
+    }
+
+    .transaction-history th {
+        background-color: #d85375;
+    }
+
+    .transaction-history tr:nth-child(even) {
+        background-color: #fff;
+    }
+
+</style>
+</style>
 <div class="wrapper">
     <div class="sidebar">
         <img src="https://bnd-s3-bucket.s3.ap-southeast-1.amazonaws.com/bnd-vertical-transparent.png" alt="Logo" id="logo">
