3e256276604e5b743a390a12a989ae8b511f2e7b
deeyanie04 <dianne2000camu@gmail.com>
2025-12-28 23:42:55 +0800

    Update transfer_to_another.php

--- transfer_to_another.php
+++ transfer_to_another.php
@@ -90,7 +90,238 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <link rel="stylesheet" href="/styles.css">
     <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
 </head>
+<style>
+    
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+  text-decoration: none;
+  font-family: 'Josefin Sans', sans-serif;
+}
 
+body{
+   background-color: #f3f5f9;
+}
+
+.wrapper{
+  display: flex;
+  position: relative;
+}
+
+.wrapper .sidebar{
+  width: 300px;
+  height: 100%;
+  background:   #e2688a;
+  padding: 30px 0px;
+  position: fixed;
+}
+
+.wrapper .sidebar ul li img{
+  width: 20px;
+  float: left;
+}
+
+.wrapper .sidebar h2{
+  color: #fff;
+  text-transform: uppercase;
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+.wrapper .sidebar ul li{
+  padding: 25px;
+  border-bottom: 1px solid #bdb8d7;
+  border-bottom: 1px solid rgba(0,0,0,0.05);
+  border-top: 1px solid rgba(255,255,255,0.05);
+  text-indent: 10px;
+}    
+  
+
+.wrapper .sidebar ul li a{
+  color: white;
+  display: block;
+}
+
+.wrapper .sidebar ul li a .fas{
+  width: 25px;
+}
+
+.wrapper .sidebar ul li:hover{
+  background-color: #d85375;
+}
+    
+.wrapper .sidebar ul li:hover a{
+  color: #fff;
+}
+ 
+.wrapper .sidebar .social_media{
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+}
+
+.wrapper .sidebar .social_media a{
+  display: block;
+  width: 40px;
+  background: #d85375;
+  height: 40px;
+  line-height: 45px;
+  text-align: center;
+  margin: 0 5px;
+  color: #fff;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.wrapper .main_content{
+  width: 100%;
+  margin-left: 300px;
+}
+
+.wrapper .main_content .header{
+  margin: 5%;
+  background: #f3f5f9;
+  color: #d85375;
+  border-bottom: 1px solid #e0e4e8;
+  text-align: center;
+}
+
+.header p{
+  color: #717171;
+  font-style: italic;
+  font-size: 13px;
+}
+
+#logo {
+  margin-left: 15% ;
+  margin-bottom: 5%;
+  max-width: 200px; 
+}
+
+
+.wrapper .main_content .transfer{
+  max-width: 100%;
+  margin: 5%;
+}
+
+.wrapper .main_content .transfer .transfer_to{
+  margin-bottom: 3%;
+  color: #e2688a;
+}
+
+.wrapper .main_content .transfer .transfer_from{
+  margin-bottom: 3%;
+  color: #e2688a;
+}
+
+
+.wrapper .main_content .transfer .transfer_amount{
+  margin-bottom: 3%;
+  color: #f3f5f9;
+  background: #e2688a;
+  border: 1px solid #e2688a;
+  padding: 3%;
+}
+
+.wrapper .main_content .transfer .transfer_button{
+  height: 10vh;
+  display: flex;
+  justify-content:center;
+  align-items: center;
+}
+
+.submit-button {
+    margin: auto;
+    padding: 15px;
+    font-size: 16px;
+    background-color:  #d85375;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.accountnum, .amount {
+      margin-top: 0.6%;
+      padding: 10px;
+      font-size: 16px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      width: 100%;
+      box-sizing: border-box;
+      display: flex;
+      background-color: #f3f3f3;
+      color: #717171;
+    }
+
+        .dropdown {
+            width: 20.6%;
+            text-decoration: none;
+            margin: auto;
+            padding: 10px;
+            font-size: 16px;
+            background-color: #d85375;
+            border: none;
+            color: #f3f5f9;
+            border-radius: 5px;
+            cursor: pointer;
+            position: relative;
+            display: inline-block;
+
+        }
+
+        .dropdown button{
+          text-decoration: none;
+          background-color: #d85375;
+          text-align: center;
+          border: none;
+          color: #f3f5f9;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #d85375;
+            min-width: 200px;
+            max-width: 80%;
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            color: #f3f5f9;
+            padding: 12px 12px;
+            text-decoration: none;
+            display: block;
+        }
+
+        .dropdown-content a:hover {
+            background-color: pink;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+
+        #outputTextField {
+                  align-items: right;
+                  justify-content: right;
+                  margin-top: 0.6%;
+                  padding: 10px;
+                  font-size: 16px;
+                  border: 1px solid #ccc;
+                  border-radius: 5px;
+                  width: 78.9%;
+                  box-sizing: border-box;
+                  background-color: #fefefa;
+                  color: #717171;
+        }
+
+
+</style>
 <body>
 
 <div class="wrapper">
