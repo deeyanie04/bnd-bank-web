f98e73860ac205548ea50896e31df187194a16fb
deeyanie04 <dianne2000camu@gmail.com>
2025-12-28 13:36:27 +0800

    Update bank.php

--- bank.php
+++ bank.php
@@ -271,14 +271,14 @@ $stmt->close();
 </style>
 <div class="wrapper">
     <div class="sidebar">
-        <img src="https://bnd-s3-bucket.s3.ap-southeast-1.amazonaws.com/bnd-vertical-transparent.png" alt="Logo" id="logo">
+        <img src="https://bnd-s3-bucket.s3.ap-southeast-1.amazonaws.com/bnd vertical transparent.png" alt="Logo" id="logo">
         <ul>
             <li><img src="https://bnd-s3-bucket.s3.ap-southeast-1.amazonaws.com/ICONS/HOME.png"><a href="#main_content">HOME</a></li>
             <li><img src="https://bnd-s3-bucket.s3.ap-southeast-1.amazonaws.com/DEPOSIT.png"><a href="deposit.php">DEPOSIT</a></li>
             <li><img src="https://bnd-s3-bucket.s3.ap-southeast-1.amazonaws.com/ICONS/WITHDRAW.png"><a href="withdraw.php">WITHDRAW</a></li>
             <li><img src="https://bnd-s3-bucket.s3.ap-southeast-1.amazonaws.com/ICONS/TRANSFER.png"><a href="transfer.php">TRANSFER</a></li>
             <li><img src="https://bnd-s3-bucket.s3.ap-southeast-1.amazonaws.com/ICONS/MORE.png"><a href="more.php">MORE</a></li>
-            <li><img src="https://bnd-s3-bucket.s3.ap-southeast-1.amazonaws.com/ICONS/LOG-OUT.png"><a href="login.php">LOG OUT</a></li>
+            <li><img src="https://bnd-s3-bucket.s3.ap-southeast-1.amazonaws.com/ICONS/LOG OUT.png"><a href="login.php">LOG OUT</a></li>
         </ul>
     </div>
 
