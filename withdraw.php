8b74764288359e0f0fc7764d4c57cda3f0222b73
deeyanie04 <dianne2000camu@gmail.com>
2025-12-28 13:45:55 +0800

    Update withdraw.php

--- withdraw.php
+++ withdraw.php
@@ -103,7 +103,181 @@ $balance = $user['balance'] ?? 0;
 <title>Withdraw / Transfer</title>
 <link rel="stylesheet" href="styles.css">
 <!-- Include your styles and JS (unchanged) -->
-</head>
+</head><style>
+    
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+  text-decoration: none;
+  font-family: 'Josefin Sans', sans-serif;
+}
+
+body{
+   background-color: #f3f5f9;
+}
+
+.wrapper{
+  display: flex;
+  position: relative;
+}
+
+.wrapper .sidebar{
+  width: 300px;
+  height: 100%;
+  background:   #e2688a;
+  padding: 30px 0px;
+  position: fixed;
+}
+
+.wrapper .sidebar ul li img{
+  width: 20px;
+  float: left;
+}
+
+.wrapper .sidebar h2{
+  color: #fff;
+  text-transform: uppercase;
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+.wrapper .sidebar ul li{
+  padding: 25px;
+  border-bottom: 1px solid #bdb8d7;
+  border-bottom: 1px solid rgba(0,0,0,0.05);
+  border-top: 1px solid rgba(255,255,255,0.05);
+  text-indent: 10px;
+}    
+
+
+.wrapper .sidebar ul li a{
+  color: white;
+  display: block;
+}
+
+.wrapper .sidebar ul li a .fas{
+  width: 25px;
+}
+
+.wrapper .sidebar ul li:hover{
+  background-color: #d85375;
+}
+    
+.wrapper .sidebar ul li:hover a{
+  color: #fff;
+}
+ 
+.wrapper .sidebar .social_media{
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+}
+
+.wrapper .sidebar .social_media a{
+  display: block;
+  width: 40px;
+  background: #d85375;
+  height: 40px;
+  line-height: 45px;
+  text-align: center;
+  margin: 0 5px;
+  color: #fff;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.wrapper .main_content{
+  width: 100%;
+  margin-left: 300px;
+}
+
+.wrapper .main_content .info{
+  margin: 20px;
+  color: #717171;
+  line-height: 25px;
+}
+.wrapper .main_content .info div{
+  margin-bottom: 20px;
+}
+
+#logo {
+  margin-left: 15% ;
+  margin-bottom: 5%;
+  max-width: 200px; 
+}
+
+
+.wrapper .main_content h2 {
+  color:  #e2688a;
+  margin-top: 10%;
+  margin-left: 5%;
+}
+
+   .textbox {
+      margin-top: 0.6%;
+      margin-left: 5%;
+      padding: 10px;
+      font-size: 16px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      width: 90%;
+      box-sizing: border-box;
+      display: inline-block;
+      background-color: #f3f3f3;
+      color: #717171;
+    }
+
+    .submit-button {
+    position: absolute;
+    justify-content: flex-end;
+    bottom: 0;
+    right: 0;
+    margin-top: 30px;
+    margin-right: 5%;
+    padding: 10px;
+    font-size: 16px;
+    background-color:  #d85375;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+.modal {
+  display: none;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 20px;
+  background-color: #e2688a; 
+  color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  text-align: center;
+}
+
+.modal-content {
+  text-align: center;
+}
+
+.modal-close {
+  background-color: #d85375; 
+  text-align: center;
+  color: #fff;
+  padding: 8px 12px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 10px; 
+}
+
+</style>
+
 <body>
 <div class="wrapper">
     <div class="sidebar">
