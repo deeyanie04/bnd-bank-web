c88464dec3e968be55f8f61ed0f7ca264742ff86
deeyanie04 <dianne2000camu@gmail.com>
2025-12-28 13:42:18 +0800

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -37,6 +37,145 @@ $s3_base = "https://bnd-s3-bucket.s3.ap-southeast-1.amazonaws.com/";
         #logo { margin-left: 15%; margin-bottom: 5%; max-width: 200px; display:block; margin:auto; }
     </style>
 </head>
+
+<style>
+    
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+  text-decoration: none;
+  font-family: 'Josefin Sans', sans-serif;
+}
+
+body{
+   background-color: #f3f5f9;
+}
+
+.wrapper{
+  display: flex;
+  position: relative;
+}
+
+.wrapper .sidebar{
+  width: 300px;
+  height: 100%;
+  background:   #e2688a;
+  padding: 30px 0px;
+  position: fixed;
+}
+
+.wrapper .sidebar ul li img{
+  width: 20px;
+  float: left;
+}
+
+.wrapper .sidebar h2{
+  color: #fff;
+  text-transform: uppercase;
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+.wrapper .sidebar ul li{
+  padding: 25px;
+  border-bottom: 1px solid #bdb8d7;
+  border-bottom: 1px solid rgba(0,0,0,0.05);
+  border-top: 1px solid rgba(255,255,255,0.05);
+  text-indent: 10px;
+}    
+  
+
+.wrapper .sidebar ul li a{
+  color: white;
+  display: block;
+}
+
+.wrapper .sidebar ul li a .fas{
+  width: 25px;
+}
+
+.wrapper .sidebar ul li:hover{
+  background-color: #d85375;
+}
+    
+.wrapper .sidebar ul li:hover a{
+  color: #fff;
+}
+ 
+.wrapper .sidebar .social_media{
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+}
+
+.wrapper .sidebar .social_media a{
+  display: block;
+  width: 40px;
+  background: #d85375;
+  height: 40px;
+  line-height: 45px;
+  text-align: center;
+  margin: 0 5px;
+  color: #fff;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.wrapper .main_content{
+  width: 100%;
+  margin-left: 300px;
+}
+
+.wrapper .main_content .header{
+  margin: 5%;
+  background: #f3f5f9;
+  color: #d85375;
+  border-bottom: 1px solid #e0e4e8;
+}
+
+.header p{
+  color: #717171;
+  font-style: italic;
+  font-size: 13px;
+}
+.wrapper .main_content .table{
+  margin: 2%;
+  width: 60%;
+  text-align: center;
+  color: #717171;
+}
+
+.wrapper .main_content .table table{
+  align-content: center;
+}
+
+.wrapper .main_content .table table td img{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 80%;
+  border: 2px solid #e2688a;
+  align-content: center;
+  border-radius: 5%;
+}
+
+.wrapper .main_content .table table td p{
+  padding: 10px;
+  cursor: pointer;
+  color: #717171;
+}
+
+#logo {
+  margin-left: 15% ;
+  margin-bottom: 5%;
+  max-width: 200px; 
+}
+
+</style>
 <body>
 
 <div class="wrapper">
